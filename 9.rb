5205be458b8cfdd7e081fe07adcde1ff3ef8036e
Ben Mathes <mathes.ben@gmail.com>
2012-01-04 09:11:15 -0800

    using the constraints given to make #9 much faster: O(n**n) instead of O(n**(n**n))

--- 9.rb
+++ 9.rb
@@ -10,20 +10,22 @@ Find the product abc.
 
 =end
 
+
+# the really stupid way of doing this, which took about 4 minutes, was to just check all triplets (a,b,c) where a, b, c < 1000
+# however, you have two restraints, namely a**2 + b**2 = c**2, and a+b+c = 1000. 
 for a in 1..1000 do
-    for b in 1..1000 do
-        for c in 1..1000 do
-            if a**2 + b**2 == c**2 then
-                puts "#{a}^2 + #{b}^2 = #{c}^2"
-                if a+b+c == 1000
-                    puts "MAGIC: #{a}^2 + #{b}^2 = #{c}^2"
-                    puts "a*b*c = #{a*b*c}"
-                    exit
-                end
-            end
+    # we subtract a since a+b+c = 1000, so b+c = 1000-a
+    for b in 1..(1000-a) do
+        # this is one of our constraints.
+        c = Math.sqrt(a**2 + b**2)
+        if a+b+c == 1000
+            puts "MAGIC: #{a}^2 + #{b}^2 = #{c}^2"
+            puts "a*b*c = #{a*b*c}"
+            exit
         end
     end
 end
+
 
 
 
