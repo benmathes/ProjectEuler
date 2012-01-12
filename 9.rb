8f104965329c0b00bf2195d12125d0175f67295e
Ben Mathes <mathes.ben@gmail.com>
2012-01-12 11:02:51 -0800

    better comments on some of the constraint utilization

--- 9.rb
+++ 9.rb
@@ -10,16 +10,21 @@ Find the product abc.
 
 =end
 
+
 cap = 1000
 
 # the really stupid way of doing this, which took about 4 minutes, was to just check all triplets (a,b,c) where a, b, c < 1000
 # however, you have two restraints, namely a**2 + b**2 = c**2, and a+b+c = 1000. 
 # addition is associative (symmetric), so we only need to chek <cap/2 against >=(cap/2)
 for a in 1..cap/2 do
-    # we subtract a since a+b+c = cap, so b+c = cap-a
+
+    # we subtract a since a+b+c = cap, so b+c = cap-a, and 
     for b in 1..(cap-a) do
-        # this is one of our constraints.
+
+        # this is one of our constraints, i.e. if we have a and b, c is fixed.
         c = Math.sqrt(a**2 + b**2)
+        
+        # Math.sqrt() might not return an int, that's what c%1 == 0 is checking (is it in N)
         if c%1 == 0 && a+b+c == cap
             puts "MAGIC: #{a}^2 + #{b}^2 = #{c}^2"
             puts "a*b*c = #{a*b*c}"
